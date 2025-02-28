f2d9ffb5bae3d7cb41875d3b3f14cd818c7602f2
alvinnick <G00438466@atu.ie>
2025-02-28 10:39:03 +0000

    comment

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -7,13 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-    presidents:string[] = ["Higgins", "McAleese", "Robinson", "Hillery", "O Dalaigh"];
+    presidents:string[] = ["Higgins", "McAleese", "Robinson", "Hillery", "O Dalaigh"];//creating array
 
     mypresidents: any[] = 
     [{name:"Higgins",term:"2011-"},
       {name:"McAleese",term:"1997-2011"},
       {name:"Robinson",term:"1990-1997"},
       {name:"Hillery",term:"1976-1990"},
-      {name:"O Dalaigh",term:"1974-1976"}
+      {name:"O Dalaigh",term:"1974-1976"}//creating array
     ];
 }
